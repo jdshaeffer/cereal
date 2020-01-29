21097fcdbc8f780d2760b82555fe3c667233686c
jdshaeffer <shaefferjd@gmail.com>
2020-01-28 23:42:26 -0700

    set up localStorage

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,28 @@
 import Head from 'next/head';
+import { useState, useEffect } from 'react';
 
 const index = () => {
+	const [count, setCount] = useState(0);
+
+	// run after the first render
+	useEffect(() => {
+		const storageData = localStorage.getItem('bowlCount');
+		if (storageData) {
+			setCount(storageData);
+		}
+	}, [])
+
+	useEffect(() => {
+		localStorage.setItem('bowlCount', count);
+	});
+
 	return (
 		<>
 			<Head>
-				<title>yahhh</title>
+				<title>cereal</title>
 			</Head>
-			<h1>sup dood</h1>
+			<p>bowls of cereal eaten: {count}</p>
+			<button onClick={() => setCount(Number(localStorage.getItem('bowlCount')) + 1)}>eat cereal</button>
 		</>
 	)
 }
