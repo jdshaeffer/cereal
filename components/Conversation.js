c5c5b31567972fcaa38e50148a8ff918aef20dd9
jdshaeffer <shaefferjd@gmail.com>
2020-03-28 20:15:20 -0600

    modular conversation

---
+++ components/Conversation.js
@@ -0,0 +1,70 @@
+const Conversation = (props) => {
+  let count = props.count;
+  let giuseppi = '';
+  let giovanni = '';
+
+	if (count >= 10) {
+		giuseppi = `woah don't you think that's enough`;
+	}
+	if (count >= 20) {
+		giuseppi = `seriously you might wanna slow it down`;
+	}
+	if (count >= 30) {
+		giuseppi = `cmon it's not worth it`;
+	}
+	if (count >= 40) {
+		giuseppi = `you're gonna hurt yourself`;
+	}
+	if (count >= 50) {
+		giuseppi = `how are you eating this much without slowin down`;
+	}
+	if (count >= 60) {
+		giuseppi = `it's actually really impressive`;
+	}
+	if (count >= 70) {
+		giuseppi = `YO GIOVANNI COME CHECK THIS GUY OUT`;
+	}
+	if (count >= 90) {
+		giovanni = `huh whatdaya want`;
+	}
+	if (count >= 105) {
+		giuseppi = `look at this guy he aint stoppin`;
+	}
+	if (count >= 110) {
+		giovanni = `woah yeah he's really throwin it down`;
+	}
+	if (count >= 115) {
+		giuseppi = `he's on bowl number ${count} and it's blowin my mind`;
+	}
+	if (count >= 120) {
+		giovanni = `what's he eatin`;
+	}
+	if (count >= 130) {
+		giuseppi = `yeah what are you eatin`;
+	}
+
+  return (
+    <>
+      <div>
+				<p>{giuseppi}</p>
+				<p>{giovanni}</p>
+      </div>
+      <style jsx>{`
+        p {
+          display: flex;
+          flex-direction: column;
+          flex-basis: auto;
+          flex: 1;
+        }
+        div {
+          display: flex;
+          flex-direction: row;
+          flex-wrap: wrap;
+          width: 100%;
+        }
+      `}</style>
+    </>
+  )
+}
+
+export default Conversation;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 import Head from 'next/head';
 import { useState, useEffect } from 'react';
 
+import Conversation from '../components/Conversation';
+
 const index = () => {
 	const [count, setCount] = useState(0);
-	let giuseppi = '';
-	let giovanni = '';
 
 	// run after the first render
 	useEffect(() => {
@@ -18,46 +18,6 @@ const index = () => {
 		localStorage.setItem('bowlCount', count);
 	});
 
-	if (count >= 10) {
-		giuseppi = `woah don't you think that's enough`;
-	}
-	if (count >= 20) {
-		giuseppi = `seriously you might wanna slow it down`;
-	}
-	if (count >= 30) {
-		giuseppi = `cmon it's not worth it`;
-	}
-	if (count >= 40) {
-		giuseppi = `you're gonna hurt yourself`;
-	}
-	if (count >= 50) {
-		giuseppi = `how are you eating this much without slowin down`;
-	}
-	if (count >= 60) {
-		giuseppi = `it's actually really impressive`;
-	}
-	if (count >= 70) {
-		giuseppi = `YO GIOVANNI COME CHECK THIS GUY OUT`;
-	}
-	if (count >= 90) {
-		giovanni = `huh whatdaya want`;
-	}
-	if (count >= 105) {
-		giuseppi = `look at this guy he aint stoppin`;
-	}
-	if (count >= 110) {
-		giovanni = `woah yeah he's really throwin it down`;
-	}
-	if (count >= 115) {
-		giuseppi = `he's on bowl number ${count} and it's blowin my mind`;
-	}
-	if (count >= 120) {
-		giovanni = `what's he eatin`;
-	}
-	if (count >= 130) {
-		giuseppi = `yeah what are you eatin`;
-	}
-
 	return (
 		<>
 			<Head>
@@ -65,24 +25,7 @@ const index = () => {
 			</Head>
 			<p>bowls of cereal eaten: {count}</p>
 			<button onClick={() => setCount(Number(localStorage.getItem('bowlCount')) + 1)}>eat cereal</button>
-			<div>
-				<p>{giuseppi}</p>
-				<p>{giovanni}</p>
-			</div>
-			<style jsx>{`
-				p {
-					display: flex;
-					flex-direction: column;
-					flex-basis: auto;
-					flex: 1;
-				}
-				div {
-					display: flex;
-					flex-direction: row;
-					flex-wrap: wrap;
-					width: 100%;
-				}
-			`}</style>
+			<Conversation count={count}/>
 		</>
 	)
 }
